db2079bc897038ec63810029f5e0f60224cedf5b
vitor <91630610+vitorserveja@users.noreply.github.com>
2025-01-31 08:37:58 -0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 cls
 mods\fabric.exe
+mkdir "%APPDATA%\.minecraft\mods"
 copy mods\sodium.jar %UserProfile%\AppData\Roaming\.minecraft\mods
 copy mods\vulkan.jar %UserProfile%\AppData\Roaming\.minecraft\mods
 cls
